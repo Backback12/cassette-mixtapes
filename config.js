87baaa10a9ab98e9fd2b9e4922b92ea7d811532f
Connor Pagtakhan <64019184+Backback12@users.noreply.github.com>
2023-05-23 05:43:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 "use strict";
 
-const SPOTIFY_CLIENT_ID = "6ae31dcc82284b73a00f589f86450bb2"
-const SPOTIFY_REDIRECT_URI = "https://backback12.github.io/spotify-cassette-mixtapes/"
+const SPOTIFY_CLIENT_ID = "6ae31dcc82284b73a00f589f86450bb2";
+const SPOTIFY_REDIRECT_URI = "https://cassettifymixtape.xyz/";
